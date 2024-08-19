99049e09c2dd431ec6f4ccf61c5a4ad3a5211c4b
mohammed khalil <dev.mohammedkhalil@gmail.com>
2024-08-19 16:50:23 +0300

    add new data base

---
+++ BEJS_Core_DataBase/dbo/Tables/_MigrationScriptsHistory.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[_MigrationScriptsHistory] (
+    [ScriptNameId]  NVARCHAR (255) NOT NULL,
+    [ExecutionDate] DATETIME2 (7)  NOT NULL,
+    [ScriptHash]    NVARCHAR (255) NOT NULL,
+    PRIMARY KEY CLUSTERED ([ScriptNameId] ASC)
+);
+
